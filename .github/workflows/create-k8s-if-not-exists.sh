1d2345c218acd26be533572f7215cb6f27fc86f2
Marcos González Andrés <marcos@binpar.com>
2022-03-11 10:36:36 +0100

    fix: apiVersions and rancher command

--- .github/workflows/create-k8s-if-not-exists.sh
+++ .github/workflows/create-k8s-if-not-exists.sh
@@ -5,5 +5,5 @@ cd ./k8s/templates
 ./compile-templates.sh "$environments"
 for environment in $environments
 do
-  rancher kubectl apply -f ../compiled/$environment.yml
+  kubectl apply -f ../compiled/$environment.yml
 done
